adc68dfefd68fffc1090e790e44957dfefa38797
umang <umangsp.1199@gmail.com>
2019-04-18 02:55:33 +0530

    message

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,13 +8,28 @@ from datetime import date
 import numpy as np
 from .models import pf_inst
 from .models import blogs
+import os
 from graphs import create
+
+def find(name, path):
+    print("finding")
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f==str(name):
+            	return os.path.join(root,str(name))
+    return "nulll"
 
 @login_required(login_url='login')
 def portfolio(request):
     if request.method=="POST":
         form = forms.csv_upload(request.POST, request.FILES)
         if form.is_valid():
+            namee=(find(request.user, "portfolio/"))
+            if namee!="nulll":
+            	os.remove(namee)
+            for filename in request.FILES:
+            	request.FILES[filename].name=str(request.user)
+            form=forms.csv_upload(request.POST, request.FILES)
             form.save()
             for filename in request.FILES:
                 name = request.FILES[filename].name
@@ -191,6 +206,10 @@ def portfolio(request):
                         #        ansy.append(JNJ.at[l,'timestamp'])
                         for l in range(0,ind+1):
                             ansx[l]=ansx[l]+(float(JNJ.at[l,'close'])*float(quant[i]))
+            print("ansy=")
+            print(ansx)
+            print(ansy)
+            form = forms.csv_upload()
             data = ansx.tolist()
             data.reverse()
             val = []
@@ -201,12 +220,208 @@ def portfolio(request):
             div_id = "mygraph1"
 
             view = create.data_plot(div_id, 'linechart', val, data_label, xlabel, ylabel)
-            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy, 'stockview':view})
+            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy, 'stockview':view, 'form':form})
+
+#            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy, 'form':form})
         else:
             return render(request,'invalid.html')
     else:
-        form = forms.csv_upload()
-        return render(request, "csv_upload.html", {'form': form})
+        if find(request.user,"portfolio/")=="nulll":
+        	form = forms.csv_upload()
+        	return render(request, "csv_upload.html", {'form': form})
+        else:
+            name = request.user
+            form = forms.csv_upload()
+            with open('portfolio/'+str(name)) as csv_file:
+                csv_reader=csv.reader(csv_file,delimiter=',')
+                comps=[]
+                dates=[]
+                quant=[]
+                ansy=[]
+                try:
+                    for row in csv_reader:
+                        comps.append(row[0])
+                        dates.append(row[1])
+                        quant.append(float(row[2]))
+                except:
+                    return render(request,'invalid.html')
+                tickers=['AAPL','BANF','CASH','FCEL','JNJ']
+                AAPL=pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=WURUTBFP9P5F15BQ&datatype=csv')
+                BANF=pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BANF&apikey=WURUTBFP9P5F15BQ&datatype=csv')
+                CASH=pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=CASH&apikey=WURUTBFP9P5F15BQ&datatype=csv')
+                FCEL=pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=FCEL&apikey=WURUTBFP9P5F15BQ&datatype=csv')
+                JNJ=pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=JNJ&apikey=WURUTBFP9P5F15BQ&datatype=csv')
+                for i in range(0,len(comps)):
+                	dates_parsed=dates[i].split("-")
+                	try:
+                		d0=date(int(dates_parsed[0]),int(dates_parsed[1]),int(dates_parsed[2]))
+                	except:
+                		return render(request,'invalid.html')
+                	if i==0:
+                		maxd=d0-d0
+                	dtemp=AAPL.at[0,'timestamp']
+                	dtemp_parsed=dtemp.split("-")
+                	dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                	if dtemp-d0>maxd:
+                		maxd=dtemp-d0
+                dtemp=AAPL.at[0,'timestamp']
+                dtemp_parsed=dtemp.split("-")
+                dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                d0=dtemp-maxd
+                ind=99
+                for j in range(0,100):
+                    dtemp=AAPL.at[j,'timestamp']
+                    dtemp_parsed=dtemp.split("-")
+                    dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                    if dtemp<d0:
+                        ind=j-1
+                        break
+                ansx=np.zeros(ind+1)
+                for l in range(0,ind+1):
+                    ansy.append(AAPL.at[l,'timestamp'])
+                for i in range(0,len(comps)):
+                    if comps[i]=="AAPL":
+                        dates_parsed=dates[i].split("-")
+                        try:
+                        	d0=date(int(dates_parsed[0]),int(dates_parsed[1]),int(dates_parsed[2]))
+                        except:
+                        	return render(request,'invalid.html')
+                        ind=99
+                        for j in range(0,100):
+                            dtemp=AAPL.at[j,'timestamp']
+                            dtemp_parsed=dtemp.split("-")
+                            dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                            if dtemp<d0:
+                                ind=j-1
+                                break
+                        #if i==0:
+                        #    ansx=np.zeros(ind+1)
+                        #    for l in range(0,ind+1):
+                        #        ansy.append(AAPL.at[l,'timestamp'])
+                        try:
+                        	if float(quant[i])<0:
+                        		return render(request, 'invalid.html')
+                        except:
+                        	return render(request, 'invalid.html')
+                        for l in range(0,ind+1):
+                            ansx[l]=ansx[l]+(float(AAPL.at[l,'close'])*float(quant[i]))
+                    elif comps[i]=="BANF":
+                        dates_parsed=dates[i].split("-")
+                        try:
+                        	d0=date(int(dates_parsed[0]),int(dates_parsed[1]),int(dates_parsed[2]))
+                        except:
+                        	return render(request,'invalid.html')
+                        ind=99
+                        for j in range(0,100):
+                            dtemp=AAPL.at[j,'timestamp']
+                            dtemp_parsed=dtemp.split("-")
+                            dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                            if dtemp<d0:
+                                ind=j-1
+                                break
+                        #if i==0:
+                        #    ansx=np.zeros(ind+1)
+                        #    for l in range(0,ind+1):
+                        #        ansy.append(BANF.at[l,'timestamp'])
+                        try:
+                        	if float(quant[i])<0:
+                        		return render(request, 'invalid.html')
+                        except:
+                        	return render(request, 'invalid.html')
+                        for l in range(0,ind+1):
+                            ansx[l]=ansx[l]+(float(BANF.at[l,'close'])*float(quant[i]))
+                    elif comps[i]=="CASH":
+                        dates_parsed=dates[i].split("-")
+                        try:
+                        	d0=date(int(dates_parsed[0]),int(dates_parsed[1]),int(dates_parsed[2]))
+                        except:
+                        	return render(request,'invalid.html')
+                        ind=99
+                        for j in range(0,100):
+                            dtemp=CASH.at[j,'timestamp']
+                            dtemp_parsed=dtemp.split("-")
+                            dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                            if dtemp<d0:
+                                ind=j-1
+                                break
+                        #if i==0:
+                        #    ansx=np.zeros(ind+1)
+                        #    for l in range(0,ind+1):
+                        #        ansy.append(CASH.at[l,'timestamp'])
+                        try:
+                        	if float(quant[i])<0:
+                        		return render(request, 'invalid.html')
+                        except:
+                        	return render(request, 'invalid.html')
+                        for l in range(0,ind+1):
+                            ansx[l]=ansx[l]+(float(CASH.at[l,'close'])*float(quant[i]))
+                    elif comps[i]=="FCEL":
+                        dates_parsed=dates[i].split("-")
+                        try:
+                        	d0=date(int(dates_parsed[0]),int(dates_parsed[1]),int(dates_parsed[2]))
+                        except:
+                        	return render(request, 'invalid.html')
+                        ind=99
+                        for j in range(0,100):
+                            dtemp=AAPL.at[j,'timestamp']
+                            dtemp_parsed=dtemp.split("-")
+                            dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                            if dtemp<d0:
+                                ind=j-1
+                                break
+                        #if i==0:
+                        #    ansx=np.zeros(ind+1)
+                        #    for l in range(0,ind+1):
+                        #        ansy.append(FCEL.at[l,'timestamp'])
+                        try:
+                        	if float(quant[i])<0:
+                        		return render(request, 'invalid.html')
+                        except:
+                        	return render(request, 'invalid.html')
+                        for l in range(0,ind+1):
+                            ansx[l]=ansx[l]+(float(FCEL.at[l,'close'])*float(quant[i]))
+                    elif comps[i]=="JNJ":
+                        dates_parsed=dates[i].split("-")
+                        try:
+                        	d0=date(int(dates_parsed[0]),int(dates_parsed[1]),int(dates_parsed[2]))
+                        except:
+                        	return render(request,'invalid.html')
+                        ind=99
+                        for j in range(0,100):
+                            dtemp=JNJ.at[j,'timestamp']
+                            dtemp_parsed=dtemp.split("-")
+                            dtemp=date(int(dtemp_parsed[0]),int(dtemp_parsed[1]),int(dtemp_parsed[2]))
+                            if dtemp<d0:
+                                ind=j-1
+                                break
+
+                        try:
+                        	if float(quant[i])<0:
+                        		return render(request, 'invalid.html')
+                        except:
+                        	return render(request, 'invalid.html')
+                        #if i==0:
+                        #    ansx=np.zeros(ind+1)
+                        #    for l in range(0,ind+1):
+                        #        ansy.append(JNJ.at[l,'timestamp'])
+                        for l in range(0,ind+1):
+                            ansx[l]=ansx[l]+(float(JNJ.at[l,'close'])*float(quant[i]))
+            print("ansy=")
+            print(ansx)
+            print(ansy)
+            data = ansx.tolist()
+            data.reverse()
+            val = []
+            val.append(data)
+            data_label = ["Portfolio"]
+            xlabel = "Time"
+            ylabel = "Portfolio Value"
+            div_id = "mygraph1"
+
+            view = create.data_plot(div_id, 'linechart', val, data_label, xlabel, ylabel)
+            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy, 'stockview':view, 'form':form})
+
+#            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy, 'form':form})
 
 
 @login_required(login_url='login')
@@ -330,8 +545,19 @@ def manually(request):
             pf.x=x
             pf.y=y
             pf.save()
+            data = ansx.tolist()
+            data.reverse()
+            val = []
+            val.append(data)
+            data_label = ["Portfolio"]
+            xlabel = "Time"
+            ylabel = "Portfolio Value"
+            div_id = "mygraph1"
 
-            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy})
+            view = create.data_plot(div_id, 'linechart', val, data_label, xlabel, ylabel)
+            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy, 'stockview':view})
+
+#            return render(request,'csv_done.html', {'r1': ansx, 'r2':ansy})
     else:
         return render(request, "add_portfolio.html")
 '''
